1602750c431bbbe766353859fb009792f3b7a5ce
Kristina <leekry@mail.ru>
2016-12-12 01:26:33 +0300

    Создали класс Меню

---
+++ Kirpich/Logic/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class Menu
+    {
+        #region Columns
+        public string NameOfDish { get; set; }
+        public string Type { get; set; }
+        public string Classification { get; set; }
+        public string Price { get; set; }
+        public string Availability { get; set; }
+        #endregion
+    }
+}
