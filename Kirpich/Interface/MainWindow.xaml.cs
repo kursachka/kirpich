7d5f44a5c1846610e9b718f24c4d56f232f2da58
kursachka <karina.u@inbox.ru>
2016-12-16 21:08:47 +0300

    added request and some buttons

--- Kirpich/Interface/MainWindow.xaml.cs
+++ Kirpich/Interface/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -29,5 +30,31 @@ namespace Interface
         {
 
         }
+
+        private void combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+                
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+       
+
+        private void buttonMenu_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Queries queries = new Queries();
+           
+                    var resMenu = queries.ShowMenu();
+                    dataGrid.ItemsSource = resMenu;
+                    
+
+           }
+        }
     }
-}
+
+
--- Kirpich/Logic/Queries.cs
+++ Kirpich/Logic/Queries.cs
@@ -6,18 +6,45 @@ using System.Threading.Tasks;
 
 namespace Logic
 {
-    class Queries
+    public class Queries
     {
         public IEnumerable<Menu> ReqForASpecifiedCategory(string category)
         {
             using (var c = new Context())
             {
                 var result = (from s in c.MainMenu.Include("MenuChapter")
-                              where s.MenuChapter.Category== category && s.Availability=="Da"
+                              where s.MenuChapter.Category == category && s.Availability == "Da"
 
                               select s).ToList();
                 return result;
 
             }
-        }      }
+        }
+        public  IEnumerable<Show_Menu> ShowMenu()
+        {
+            using (var c=new Context())
+            {
+                var menu = (from s in c.MainMenu.Include("MenuChapter")
+                            select new Show_Menu
+                            {
+                                Name=s.NameOfDish,
+                                Type=s.MenuChapter.Category,
+                                Price=s.Price,
+                                YesNo=s.Availability
+                            }
+                            ).ToList();
+                return menu;
+
+            }
+        }
+
+    }
+    public class Show_Menu
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Price { get; set; }
+        public string YesNo { get; set; }
+
+    }
 }
