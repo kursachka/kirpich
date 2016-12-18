97c9d597fe7baea87b64d0c34221aab3c6925068
Kristina <leekry@mail.ru>
2016-12-18 22:59:59 +0300

    У нас будет красивый дизайн (нет)

---
+++ Kirpich/WcfServiceLibrary1/IService1.vb
@@ -0,0 +1,27 @@
+﻿' ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в контекстном меню, чтобы изменить имя интерфейса "IService1" в коде и файле конфигурации.
+<ServiceContract()>
+Public Interface IService1
+
+    <OperationContract()>
+    Function GetData(ByVal value As Integer) As String
+
+    <OperationContract()>
+    Function GetDataUsingDataContract(ByVal composite As CompositeType) As CompositeType
+
+    ' TODO: Добавьте здесь операции служб
+
+End Interface
+
+' Используйте контракт данных, как показано на следующем примере, чтобы добавить сложные типы к сервисным операциям.
+' В проект можно добавлять XSD-файлы. После построения проекта, вы можете напрямую использовать в нем определенные типы данных с пространством имен "WcfServiceLibrary1.ContractType".
+
+<DataContract()>
+Public Class CompositeType
+
+    <DataMember()>
+    Public Property BoolValue() As Boolean
+
+    <DataMember()>
+    Public Property StringValue() As String
+
+End Class
---
+++ Kirpich/WcfServiceLibrary1/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("WcfServiceLibrary1")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("WcfServiceLibrary1")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Указанный ниже идентификатор GUID предназначен для идентификации библиотеки типов, если этот проект будет видимым для COM-объектов
+<Assembly: Guid("469468a5-cdf9-4909-b16a-500d8f841065")> 
+
+' Сведения о версии сборки состоят из указанных ниже четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии 
+'      Номер сборки
+'      Номер редакции
+'
+' Можно указать все значения или установить для номеров редакции и сборки значение по умолчанию 
+' с помощью символа "*", как показано ниже:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
