98edee3a87d895d53819ccabdf93505933e698b8
kursachka <karina.u@inbox.ru>
2016-12-18 21:42:57 +0300

    Пофиксили check

--- Kirpich/Interface/MainWindow.xaml.cs
+++ Kirpich/Interface/MainWindow.xaml.cs
@@ -1,59 +1,40 @@
 ﻿using Logic;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Data.OleDb;
 using Logic.Show_Menu_All;
+using System;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace Interface
 {
-  
-    
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
-       
+
+        int sum = 0;
         Queries queries = new Queries();
         public MainWindow()
         {
             InitializeComponent();
         }
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
 
-        }
-
-       
+        
 
         private void buttonMenu_Click(object sender, RoutedEventArgs e)
         {
-                
-                    var resMenu = queries.ShowMenu();
-                    dataGrid.ItemsSource = resMenu;
-                    dataGrid.Columns[0].IsReadOnly = true;
-                    dataGrid.Columns[1].IsReadOnly = true;
-                    dataGrid.Columns[2].IsReadOnly = true;
-                    dataGrid.Columns[3].IsReadOnly = true;
-                    tbCena.Text = "";
+
+            var resMenu = queries.ShowMenu();
+            dataGrid.ItemsSource = resMenu;
+            dataGrid.Columns[0].IsReadOnly = true;
+            dataGrid.Columns[1].IsReadOnly = true;
+            dataGrid.Columns[2].IsReadOnly = true;
+            dataGrid.Columns[3].IsReadOnly = true;
+            tbCena.Text = "";
         }
 
 
 
         private void buttonComlex_Click(object sender, RoutedEventArgs e)
-        { 
+        {
 
             var resComplex = queries.ComplexDinner();
             dataGrid.ItemsSource = resComplex;
@@ -69,29 +50,46 @@ namespace Interface
         private void CurrentCellChanged(object sender, EventArgs e)
         {
             var resMenu = dataGrid.ItemsSource;
-
-            int sum = 0;
-
-
-            foreach (Show_Menu_Available item in resMenu)
+      
+            try
             {
-                if (item.Check == true)
-                { sum = sum + item.Price; }
-                tbCena.Text = sum.ToString();
+                foreach (Show_Menu item in resMenu)
+                {
+                    if (item.Check == true)
+                    { sum = sum + item.Price; } 
+                }
+                
             }
-
-
+            catch
+            {
+                try
+                {
+                    foreach (Show_Menu_Available item in resMenu)
+                    {
+                        if (item.Check == true)
+                        { sum = sum + item.Price; }
+                   
+                    }
+                }
+                catch
+                {
+                    foreach (Show_Menu_Category item in resMenu)
+                    {
+                        if (item.Check == true)
+                        { sum = sum + item.Price; }
+                        
+                    }
+                }
+            }
         }
-            
+
 
         private void buttonAvailable_Click(object sender, RoutedEventArgs e)
         {
             var resAvailable = queries.ReqForAvailable();
             dataGrid.ItemsSource = resAvailable;
-            
-            
         }
-                    
+
         private void buttonFirstCourse_Click(object sender, RoutedEventArgs e)
         {
             var resFirstCourse = queries.ReqForASpecifiedCategory("Pervoe bljudo");
@@ -114,8 +112,11 @@ namespace Interface
         {
             var resDeserts = queries.ReqForASpecifiedCategory("Desert");
             dataGrid.ItemsSource = resDeserts;
-           }
         }
+
     }
+}    
+    
+
 
 
