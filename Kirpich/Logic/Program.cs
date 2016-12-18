976684f8d14c89228b3212230af5f98d8ebc9cbc
kursachka <karina.u@inbox.ru>
2016-12-18 23:53:22 +0300

    В общем, vk api я подключила, но не успела доделать:(

--- Kirpich/Logic/Program.cs
+++ Kirpich/Logic/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data.Entity.Migrations;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,8 +12,21 @@ namespace Logic
 {
     class Program
     {
+       
+            public static string MessageSendVK(string user_id)
+        {
+            string key = "4b72a0dec3ca9684a2725e73ce50ea5a76c7936879857397b86d73b0b728a943df38cbe0897b10aedfeae";
+            string messageText = "Карина налажала и поздно села за VK API. Не ставьте нолик, пожалуйста:((((";
+
+            return string.Format("https://api.vk.com/method/messages.send?id&domain={0}&message={1}&access_token={2}&v=5.60", user_id, messageText, key);
+        }
         static void Main(string[] args)
         {
+
+            using (var client=new HttpClient())
+            {
+                string result = client.GetStringAsync(MessageSendVK("sergefr")).Result;
+            }
         }
     }
 }
--- Kirpich/Logic/Repository.cs
+++ Kirpich/Logic/Repository.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ namespace Logic
 {
     public class Repository
     {
+        
         private class ReadData
         {
             public List<Menu> MainMenu { get; set; }
@@ -22,7 +24,7 @@ namespace Logic
         {
             get { return readData.MainMenu; }
             private set { }
-           
+
         }
 
         public IEnumerable<MenuChapter> MenuChapters
@@ -34,15 +36,38 @@ namespace Logic
 
         public Repository()
         {
+
             var jstring = File.ReadAllText(("Menu.json"));
-        //    var readDD = JsonConvert.DeserializeObject(jstring);
+            //    var readDD = JsonConvert.DeserializeObject(jstring);
             readData = JsonConvert.DeserializeObject<ReadData>(jstring);
             MainMenu = readData.MainMenu;
             MenuChapters = readData.MenuChapters;
         }
 
-           
-        
+        //public static string GetNewMessage()
+        //{
+        //    string key = "4b72a0dec3ca9684a2725e73ce50ea5a76c7936879857397b86d73b0b728a943df38cbe0897b10aedfeae";
+        //    return string.Format("https://api.vk.com/method/messages.get?&out=0&access_token={2}&v=5.60", key);
+        //}
+
+        //public static string MessageSendVK(string user_id)
+        //{
+        //    string key = "4b72a0dec3ca9684a2725e73ce50ea5a76c7936879857397b86d73b0b728a943df38cbe0897b10aedfeae";
+        //    string messageText = "Карина налажала и поздно села за VK API. Не ставьте нолик, пожалуйста:((((";
+
+        //    return string.Format("https://api.vk.com/method/messages.send?id&domain={0}&message={1}&access_token={2}&v=5.60", user_id, messageText, key);
+
+
+            //}
+            //public static string GetContent()
+            //{
+
+            //    using (var client = new HttpClient())
+            //    {
+            //        var result = client.GetStringAsync(()).Result;
+            //        return JsonConvert.DeserializeObject<ResponseVkMessage>(result);
+            //    }
+            //}
 
 
 
@@ -50,10 +75,5 @@ namespace Logic
 
 
 
-    }
-
-
-
-
-
+        }
 }
